8848afcf8fcde91d18d999a858a11ae555ea0e48
bradleyGamiMarques <bradleygamimarques@outlook.com>
2024-05-26 19:27:39 -0700

    [refactor] Create base type P3RPersona
    - Make response types aliases for base type.
    - Add IsDLC field.

--- types.go
+++ types.go
@@ -1,25 +1,16 @@
 package types
 
-type GetP3RPersonaByNameResponse struct {
+type P3RPersona struct {
 	Agility      int    `json:"agility"`
 	Arcana       string `json:"arcana"`
 	Background   string `json:"background"`
 	Endurance    int    `json:"endurance"`
+	IsDLC        *bool  `json:"isDLC,omitempty"`
 	Luck         int    `json:"luck"`
 	Magic        int    `json:"magic"`
 	PersonaName  string `json:"personaName"`
 	PersonaLevel int    `json:"personaLevel"`
 	Strength     int    `json:"strength"`
 }
-
-type GetP3RPersonasByArcanaResponse []struct {
-	Agility      int    `json:"agility"`
-	Arcana       string `json:"arcana"`
-	Background   string `json:"background"`
-	Endurance    int    `json:"endurance"`
-	Luck         int    `json:"luck"`
-	Magic        int    `json:"magic"`
-	PersonaName  string `json:"personaName"`
-	PersonaLevel int    `json:"personaLevel"`
-	Strength     int    `json:"strength"`
-}
+type GetP3RPersonaByNameResponse P3RPersona
+type GetP3RPersonasByArcanaResponse []P3RPersona
