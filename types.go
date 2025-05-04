f054962097b2a435406f08a1527b0d852312a575
Bradley Marques <bradleygamimarques@gmail.com>
2025-05-03 20:44:35 -0700

    Feat: add request and response type
    - Add GetP3RPersonasResponse type
    - Add GetP3RPersonasRequest type
    
    These types allow the caller to return a concise form of a P3RPersona struct for use in a list such as on a landing page

--- types.go
+++ types.go
@@ -1,14 +1,24 @@
 package types
 
 type P3RPersona struct {
-	Agility      int    `json:"agility"`
-	Arcana       string `json:"arcana"`
-	Endurance    int    `json:"endurance"`
-	Luck         int    `json:"luck"`
-	Magic        int    `json:"magic"`
 	PersonaName  string `json:"personaName"`
+	Arcana       string `json:"arcana"`
 	PersonaLevel int    `json:"personaLevel"`
 	Strength     int    `json:"strength"`
+	Magic        int    `json:"magic"`
+	Endurance    int    `json:"endurance"`
+	Agility      int    `json:"agility"`
+	Luck         int    `json:"luck"`
+}
+type P3RPersonaListItem struct {
+	PersonaName  string `json:"personaName"`
+	Arcana       string `json:"arcana"`
+	PersonaLevel int    `json:"personaLevel"`
+	IsDLC        bool   `json:"isDLC"`
 }
 type GetP3RPersonaByNameResponse P3RPersona
 type GetP3RPersonasByArcanaResponse []P3RPersona
+type GetP3RPersonasResponse []P3RPersonaListItem
+type GetP3RPersonasRequest struct {
+	Arcanas []string `json:"arcanas"`
+}
